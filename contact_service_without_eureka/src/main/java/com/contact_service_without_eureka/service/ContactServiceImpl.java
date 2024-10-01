2834bd7d4852d0281a26dc7f8ae21c78cd6a7a15
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2024-10-01 19:26:22 +0530

    Commit changes...

--- contact_service_without_eureka/src/main/java/com/contact_service_without_eureka/service/ContactServiceImpl.java
+++ contact_service_without_eureka/src/main/java/com/contact_service_without_eureka/service/ContactServiceImpl.java
@@ -11,22 +11,22 @@ public class ContactServiceImpl implements ContactService {
 	private List<Contact> listContact;
 
 	public ContactServiceImpl(List<Contact> listContact) {
-		super();		
+		super();
 	}
 
 	public ContactServiceImpl() {
-		super();		
+		super();
 		listContact = List.of(
-				new Contact(1L, "c1@gail.com", "c1", 1311L),
-				new Contact(2L, "c2@gail.com", "c2", 1311L), 
-				new Contact(3L, "c3@gail.com", "c3", 1312L),
-				new Contact(4L, "c4@gail.com", "c4", 1313L));
-		// TODO Auto-generated constructor stub
+				new Contact(1L, "c1@gmail.com", "c1", 1311L), 
+				new Contact(2L, "c2@gmail.com", "c2", 1311L),
+				new Contact(3L, "c3@gmail.com", "c3", 1312L), 
+				new Contact(4L, "c4@gmail.com", "c4", 1313L));
+
 	}
 
 	@Override
 	public List<Contact> getContactsOfUser(Long userId) {
-		// TODO Auto-generated method stub
+
 		return listContact.stream().filter(contact -> contact.getUserId().equals(userId)).collect(Collectors.toList());
 	}
 
