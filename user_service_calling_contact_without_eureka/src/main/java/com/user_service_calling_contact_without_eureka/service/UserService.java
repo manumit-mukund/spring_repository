1c253e55f4949998b457afeb85d1c24dc1fb7720
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2024-10-01 18:54:21 +0530

    First commit...

---
+++ user_service_calling_contact_without_eureka/src/main/java/com/user_service_calling_contact_without_eureka/service/UserService.java
@@ -0,0 +1,14 @@
+package com.user_service_calling_contact_without_eureka.service;
+
+import java.util.List;
+import com.user_service_calling_contact_without_eureka.model.User;
+
+public interface UserService {
+
+	public User getUser(Long id);
+
+	public List<User> getAllUsers();
+
+	public User addUser(User user);
+
+}
---
+++ user_service_calling_contact_without_eureka/src/main/java/com/user_service_calling_contact_without_eureka/service/UserServiceImpl.java
@@ -0,0 +1,43 @@
+package com.user_service_calling_contact_without_eureka.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Service;
+import com.user_service_calling_contact_without_eureka.model.User;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	private List<User> listUser;
+
+	public UserServiceImpl() {
+		super();
+		listUser = new ArrayList<User>();
+		listUser.add(new User(1311L, "ABC", "123"));
+		listUser.add(new User(1312L, "XYZ", "124"));
+		listUser.add(new User(1313L, "PQR", "125"));
+
+	}
+
+	@Override
+	public User getUser(Long id) {
+
+		return listUser.stream().filter(user -> user.getUserId().equals(id)).findAny().orElse(null);
+
+	}
+
+	@Override
+	public List<User> getAllUsers() {
+
+		return listUser;
+	}
+
+	@Override
+	public User addUser(User user) {
+
+		listUser.add(user);
+		return user;
+
+	}
+
+}
