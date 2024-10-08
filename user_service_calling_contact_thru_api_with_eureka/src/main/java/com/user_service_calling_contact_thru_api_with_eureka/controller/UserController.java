0c5ee6e68229ba806902d7bf2eaf26adb5e3be31
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2024-10-08 22:44:56 +0530

    Commit changes..

--- user_service_calling_contact_thru_api_with_eureka/src/main/java/com/user_service_calling_contact_thru_api_with_eureka/controller/UserController.java
+++ user_service_calling_contact_thru_api_with_eureka/src/main/java/com/user_service_calling_contact_thru_api_with_eureka/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.user_service_calling_contact_thru_api_with_eureka.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
 import com.user_service_calling_contact_thru_api_with_eureka.model.Contact;
 import com.user_service_calling_contact_thru_api_with_eureka.model.User;
 import com.user_service_calling_contact_thru_api_with_eureka.service.UserService;
