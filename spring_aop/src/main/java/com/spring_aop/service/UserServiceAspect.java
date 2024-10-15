bbbce4db0c19f778d5ce07c4186cdad470ca2800
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2024-10-15 17:22:17 +0530

    Commit changes...

--- spring_aop/src/main/java/com/spring_aop/service/UserServiceAspect.java
+++ spring_aop/src/main/java/com/spring_aop/service/UserServiceAspect.java
@@ -4,7 +4,6 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Service;
@@ -14,14 +13,14 @@ import org.springframework.stereotype.Service;
 @Aspect
 public class UserServiceAspect {
 
-	@Before("execution(* com.spring_aop.controller..*(..))")
+	@Before("execution(* com.spring_aop.controller.UserController.getAllUsers(..))")
 	public void myBeforeAdvice(JoinPoint jp) {
 
 		System.out.println("myBeforeAdvice called.");
 
 	}
 
-	@After("execution(* com.spring_aop.controller..*(..))")
+	@After("execution(* com.spring_aop.controller.UserController.hello(..))")
 	public void myAfterAdvice(JoinPoint jp) {
 
 		System.out.println("myAfterAdvice called.");
@@ -35,7 +34,7 @@ public class UserServiceAspect {
 
 	}
 
-	@Around("execution(* com.spring_aop.controller..*(..))")
+	// @Around("execution(* com.spring_aop.controller..*(..))")
 	public void myAroundAdvice(ProceedingJoinPoint jp) {
 
 		System.out.println("myAroundAdvice called.");
