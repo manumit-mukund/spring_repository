c9038bdadfc3d80defef0f621284b07875c339ac
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2024-10-15 10:08:13 +0530

    First commit...

---
+++ spring_core/src/main/java/com/spring_core/service/StudentService.java
@@ -0,0 +1,31 @@
+package com.spring_core.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import com.spring_core.model.Student;
+
+public class StudentService {
+
+	@Autowired
+	@Qualifier("student2")
+	private Student student;
+
+	public StudentService() {
+
+		System.out.println("Inside Profile constructor.");
+
+	}
+
+	public void printAge() {
+
+		System.out.println("Age : " + student.getAge());
+
+	}
+
+	public void printName() {
+
+		System.out.println("Name : " + student.getName());
+
+	}
+}
