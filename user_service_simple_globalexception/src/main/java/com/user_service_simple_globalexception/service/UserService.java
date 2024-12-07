295a57114a0baaaeb7314cba9078d8bb34eade72
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2024-12-07 18:52:38 +0530

    First commit...

---
+++ user_service_simple_globalexception/src/main/java/com/user_service_simple_globalexception/service/UserService.java
@@ -0,0 +1,15 @@
+package com.user_service_simple_globalexception.service;
+
+import java.util.List;
+
+import com.user_service_simple_globalexception.model.User;
+
+public interface UserService {
+
+	public User getUser(String username);
+
+	public List<User> getAllUsers();
+
+	public User addUser(User user);
+
+}
