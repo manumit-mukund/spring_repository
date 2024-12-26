fdbf10220f634e4e12c46307ab34e6d9ec95966a
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2024-12-26 22:45:59 +0530

    Commit changes...

---
+++ user_service_simple_log_using_lombok/src/main/java/com/user_service_simple_log_using_lombok/controller/ExampleController.java
@@ -0,0 +1,34 @@
+package com.user_service_simple_log_using_lombok.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequestMapping("/api")
+@Slf4j
+public class ExampleController {
+
+	@GetMapping("/test")
+	public String testLogging() {
+		
+		log.info("An informational message");
+
+		try {
+
+			// Simulate an error
+			log.debug("A debug message");
+			throw new RuntimeException("Test exception");
+
+		} catch (RuntimeException e) {
+
+			// Log at different levels		
+			log.warn("A warning message", e);
+			log.error("An error message", e);
+			throw e;
+
+		}
+	}
+
+}
--- user_service_simple_log_using_lombok/src/main/java/com/user_service_simple_log_using_lombok/service/UserServiceImpl.java
+++ user_service_simple_log_using_lombok/src/main/java/com/user_service_simple_log_using_lombok/service/UserServiceImpl.java
@@ -2,16 +2,14 @@ package com.user_service_simple_log_using_lombok.service;
 
 import java.util.ArrayList;
 import java.util.List;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
 import com.user_service_simple_log_using_lombok.model.User;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
 public class UserServiceImpl implements UserService {
 
-	
 	private List<User> listUser;
 
 	public UserServiceImpl() {
@@ -24,7 +22,9 @@ public class UserServiceImpl implements UserService {
 		listUser.add(new User("abc", "abc", "abc@yahoo.com"));
 		listUser.add(new User("xyz", "xyz", "xyz@yahoo.com"));
 
-		log.debug("Action completed successfully UserServiceImpl().");
+		log.debug("debug: Action completed successfully UserServiceImpl().");
+		log.warn("warn: Action completed successfully UserServiceImpl().");
+		log.error("error: Action completed successfully UserServiceImpl().");
 
 	}
 
