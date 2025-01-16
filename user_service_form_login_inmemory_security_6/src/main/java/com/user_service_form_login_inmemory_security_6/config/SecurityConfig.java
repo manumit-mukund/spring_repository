ba40baaa1597b8cd3c0f7cf2305f35202420a49a
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-01-16 12:27:24 +0530

    First commit...

---
+++ user_service_form_login_inmemory_security_6/src/main/java/com/user_service_form_login_inmemory_security_6/config/SecurityConfig.java
@@ -0,0 +1,62 @@
+package com.user_service_form_login_inmemory_security_6.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+		http
+		.csrf()
+		.disable()
+		.authorizeHttpRequests((authorize) -> {
+			
+			authorize.requestMatchers("api/v1/**").permitAll();
+			authorize.anyRequest().authenticated();
+		})
+		.httpBasic(Customizer.withDefaults());
+		
+		return http.build();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+
+		UserDetails john = User
+				.builder()
+				.username("user1")
+				.password(passwordEncoder().encode("user1"))
+				.roles("USER")
+				.build();
+
+		UserDetails sam = User
+				.builder()
+				.username("admin1")
+				.password(passwordEncoder().encode("admin1"))
+				.roles("ADMIN")
+				.build();
+
+		return new InMemoryUserDetailsManager(john, sam);
+	}
+}
