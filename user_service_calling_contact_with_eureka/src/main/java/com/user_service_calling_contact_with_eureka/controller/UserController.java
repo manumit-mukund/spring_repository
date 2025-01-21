bfe39c6e1c48c1844f8d2839f4edc49146cec8b6
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-01-21 18:05:37 +0530

    Commit changes...

--- user_service_calling_contact_with_eureka/src/main/java/com/user_service_calling_contact_with_eureka/controller/UserController.java
+++ user_service_calling_contact_with_eureka/src/main/java/com/user_service_calling_contact_with_eureka/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.user_service_calling_contact_with_eureka.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.user_service_calling_contact_with_eureka.model.Contact;
 import com.user_service_calling_contact_with_eureka.model.User;
 import com.user_service_calling_contact_with_eureka.service.UserService;
@@ -46,6 +49,26 @@ public class UserController {
 
 	@GetMapping("/getall")
 	public List<User> getAllUsers() {
+		
+		String serviceUri = discoveryClient.getInstances("contact_service_with_eureka").get(0).getUri().toString();
+		
+		List<Contact> listContactResult = restTemplate.getForObject(serviceUri + "/contact/getall", List.class);
+
+		ObjectMapper mapper = new ObjectMapper();
+		
+		List<Contact> listContact = mapper.convertValue(listContactResult, new TypeReference<List<Contact>>() {
+		});
+
+		for (User user : userService.getAllUsers()) {
+
+			Long userId = user.getUserId();
+
+			user.setContacts(listContact
+					.stream()
+					.filter(contact -> contact.getUserId().equals(userId))
+					.collect(Collectors.toList())
+					);
+		}
 
 		return userService.getAllUsers();
 	}
