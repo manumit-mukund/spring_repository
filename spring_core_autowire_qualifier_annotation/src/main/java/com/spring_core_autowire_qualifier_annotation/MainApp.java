35a9ef957a41f0c0c7afed27839f24e9568ff59c
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-02-13 22:48:57 +0530

    First commit...

---
+++ spring_core_autowire_qualifier_annotation/src/main/java/com/spring_core_autowire_qualifier_annotation/MainApp.java
@@ -0,0 +1,19 @@
+package com.spring_core_autowire_qualifier_annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.spring_core_autowire_qualifier_annotation.model.Profile;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+
+		Profile profile = (Profile) context.getBean("profile");
+		profile.printAge();
+		profile.printName();
+
+	}
+}
---
+++ spring_core_autowire_qualifier_annotation/src/main/java/com/spring_core_autowire_qualifier_annotation/model/Student.java
@@ -0,0 +1,23 @@
+package com.spring_core_autowire_qualifier_annotation.model;
+
+public class Student {
+
+	private Integer age;
+	private String name;
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
