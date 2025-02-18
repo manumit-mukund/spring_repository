091a667e10eb47cd76c95856a123b45e8fc1feca
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-02-18 14:59:49 +0530

    Commit changes...

--- spring_core_bean_scope_with_annotation/src/main/java/com/spring_core_bean_scope_with_annotation/controller/PrototypeController.java
+++ spring_core_bean_scope_with_annotation/src/main/java/com/spring_core_bean_scope_with_annotation/controller/PrototypeController.java
@@ -18,14 +18,14 @@ public class PrototypeController {
 	@GetMapping("/prototype")
 	public String usePrototype() {
 
-		PrototypeBean prototypeBean1 = context.getBean(PrototypeBean.class);	
-		
-		prototypeBean1.setState("Updated State at " + new Date());		
+		PrototypeBean prototypeBean1 = context.getBean(PrototypeBean.class);
+
+		prototypeBean1.setState("Updated State at " + new Date());
 		System.out.println("\nprototypeBean1.getState()  = " + prototypeBean1.getState());
-		
-		PrototypeBean prototypeBean2 = context.getBean(PrototypeBean.class);	
+
+		PrototypeBean prototypeBean2 = context.getBean(PrototypeBean.class);
 		System.out.println("\nprototypeBean2.getState()  = " + prototypeBean2.getState());
-		
+
 		return "returned prototypeBean2.getState() =" + prototypeBean2.getState();
 
 	}
--- spring_core_bean_scope_with_annotation/src/main/java/com/spring_core_bean_scope_with_annotation/controller/SingletonController.java
+++ spring_core_bean_scope_with_annotation/src/main/java/com/spring_core_bean_scope_with_annotation/controller/SingletonController.java
@@ -7,7 +7,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.spring_core_bean_scope_with_annotation.component.PrototypeBean;
 import com.spring_core_bean_scope_with_annotation.component.SingletonBean;
 
 @RestController
@@ -21,12 +20,12 @@ public class SingletonController {
 
 		SingletonBean singletonBean1 = context.getBean(SingletonBean.class);
 
-		singletonBean1.setState("Updated State at " + new Date());		
+		singletonBean1.setState("Updated State at " + new Date());
 		System.out.println("\nsingletonBean1.getState()  = " + singletonBean1.getState());
-		
-		SingletonBean singletonBean2 = context.getBean(SingletonBean.class);	
+
+		SingletonBean singletonBean2 = context.getBean(SingletonBean.class);
 		System.out.println("\nsingletonBean2.getState()  = " + singletonBean2.getState());
-		
+
 		return "returned singletonBean2.getState() =" + singletonBean2.getState();
 
 	}
