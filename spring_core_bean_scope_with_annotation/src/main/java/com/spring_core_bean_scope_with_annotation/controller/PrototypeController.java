a3ccfcc84c08a01dfc81ba3cc6202abb6d4f83b0
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-04-12 18:21:19 +0530

    Commit changes...

--- spring_core_bean_scope_with_annotation/src/main/java/com/spring_core_bean_scope_with_annotation/controller/PrototypeController.java
+++ spring_core_bean_scope_with_annotation/src/main/java/com/spring_core_bean_scope_with_annotation/controller/PrototypeController.java
@@ -19,10 +19,10 @@ public class PrototypeController {
 	public String usePrototype() {
 
 		PrototypeBean prototypeBean1 = context.getBean(PrototypeBean.class);
-		prototypeBean1.setState("Updated State at " + new Date());		
+		prototypeBean1.setState("Updated State at " + new Date());
 		System.out.println("\nprototypeBean1.getState()  = " + prototypeBean1.getState());
 
-		PrototypeBean prototypeBean2 = context.getBean(PrototypeBean.class);		
+		PrototypeBean prototypeBean2 = context.getBean(PrototypeBean.class);
 		System.out.println("\nprototypeBean2.getState()  = " + prototypeBean2.getState());
 
 		return "returned prototypeBean2.getState() =" + prototypeBean2.getState();
