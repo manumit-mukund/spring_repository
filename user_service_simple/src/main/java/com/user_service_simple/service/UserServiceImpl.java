ceab9dde49a3803dec523e6dc4beb79dc947cd36
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-05-18 14:38:20 +0530

    Commit changes...

--- user_service_simple/src/main/java/com/user_service_simple/service/UserServiceImpl.java
+++ user_service_simple/src/main/java/com/user_service_simple/service/UserServiceImpl.java
@@ -17,6 +17,7 @@ public class UserServiceImpl implements UserService {
 		super();
 
 		listUser = new ArrayList<User>();
+		
 		listUser.add(new User("abc", "abc", "abc@yahoo.com"));
 		listUser.add(new User("xyz", "xyz", "xyz@yahoo.com"));
 
