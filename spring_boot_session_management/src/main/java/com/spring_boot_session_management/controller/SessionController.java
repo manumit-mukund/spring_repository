2235818ea8a6bb4a6fc6aa6fb83990a1306126f0
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-05-25 13:33:31 +0530

    First commit...

---
+++ spring_boot_session_management/src/main/java/com/spring_boot_session_management/controller/SessionController.java
@@ -0,0 +1,45 @@
+package com.spring_boot_session_management.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpSession;
+
+@RestController
+@RequestMapping("/session")
+public class SessionController {
+
+	@GetMapping("/create")
+	public String createSession(HttpSession session) {
+
+		session.setAttribute("username", "Manu");
+
+		String sessionId = session.getId();
+
+		return "Session created with ID: " + sessionId;
+
+	}
+
+	@GetMapping("/get")
+	public String getSession(HttpSession session) {
+
+		String username = (String) session.getAttribute("username");
+
+		if (username == null) {
+			return "No session found!";
+		}
+
+		return "Session found with username: " + username;
+
+	}
+
+	@GetMapping("/invalidate")
+	public String invalidateSession(HttpSession session) {
+
+		session.invalidate();
+
+		return "Session invalidated!";
+
+	}
+}
---
+++ spring_boot_session_management/src/test/java/com/spring_boot_session_management/SpringBootTransactionManagementTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot_session_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTransactionManagementTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
