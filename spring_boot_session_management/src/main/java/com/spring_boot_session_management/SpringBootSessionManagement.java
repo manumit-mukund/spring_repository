997436e012e9af361daeb9dd1531587a7b193de6
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-05-25 15:22:11 +0530

    Commit changes...

--- spring_boot_session_management/src/main/java/com/spring_boot_session_management/SpringBootSessionManagement.java
+++ spring_boot_session_management/src/main/java/com/spring_boot_session_management/SpringBootSessionManagement.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootTransactionManagement {
+public class SpringBootSessionManagement {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(SpringBootTransactionManagement.class, args);
+		SpringApplication.run(SpringBootSessionManagement.class, args);
 
 	}
 
