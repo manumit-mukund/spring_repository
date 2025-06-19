9b81ab5d3290941e7ef2a78f73776c6bb6d38fcd
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-06-19 20:09:00 +0530

    Commit changes...

--- spring_boot_redis_session_management/src/main/java/com/spring_boot_redis_session_management/controller/SessionController.java
+++ spring_boot_redis_session_management/src/main/java/com/spring_boot_redis_session_management/controller/SessionController.java
@@ -4,27 +4,68 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/redis")
 public class SessionController {
 
-	@GetMapping("/session")
-	public String manageSession(HttpSession session) {
+	@GetMapping("/session/set")
+	public String setSession(HttpSession session) {
 
-		String sessionId = session.getId();
+		// String sessionId = session.getId();
 
-		String attribute = (String) session.getAttribute("myAttribute");
+		String sessionId = "";
+		String username = "";
 
-		if (attribute == null) {
+		if (session.isNew()) {
 
-			session.setAttribute("myAttribute", "Manu");
+			sessionId = "New session created: " + session.getId();
 
-			attribute = (String) session.getAttribute("myAttribute");
+			session.setAttribute("username", "Manu");
+			username = (String) session.getAttribute("username");
+
+		} else {
+
+			sessionId = "Existing session accessed: " + session.getId();
+			username = (String) session.getAttribute("username");
+		}
+
+		return "Session ID: " + sessionId + ", username: " + username;
+
+		// Test url: http://localhost:9001/redis/session/set
+	}
+
+	@GetMapping("/session/get")
+	public String getSession(HttpServletRequest request) {
+
+		HttpSession session = request.getSession(false); // Get existing session, don't create new if none exists
+
+		String sessionId = "Session ID not found, session is new";
+		String username = "username not found";
+
+		if (session != null) {
+
+			sessionId = session.getId();
+			username = (String) session.getAttribute("username");
 
 		}
 
-		return "Session ID: " + sessionId + ", Attribute: " + attribute;
+		return "Session ID: " + sessionId + ", username: " + username;
+
+		// Test url: http://localhost:9001/redis/session/get
+	}
+
+	@GetMapping("/session/invalidate")
+	public String invalidateSession(HttpSession session) {
+
+		session.invalidate();
+
+		String sessionInvalidated = "session invalidated";
+
+		return sessionInvalidated;
+
+		// Test url: http://localhost:9001/redis/session/invalidate
 	}
 }
