f6002e4f975408f345f5b2bb51f6dc38b4506593
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-06-26 10:48:04 +0530

    Commit changes...

--- spring_boot_session_management/src/main/java/com/spring_boot_session_management/controller/SessionController.java
+++ spring_boot_session_management/src/main/java/com/spring_boot_session_management/controller/SessionController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 @RestController
@@ -13,35 +14,120 @@ public class SessionController {
 	@GetMapping("/create")
 	public String createSession(HttpSession session) {
 
-		session.setAttribute("username", "Manu");
+		String sessionId = "";
+		String username1 = "";
+		String username2 = "";
+		String username3 = "";
 
-		String sessionId = session.getId();
+		if (session.isNew()) {
 
-		return "Session created with ID: " + sessionId;
+			sessionId = "New session created: " + session.getId();
+
+			session.setAttribute("username1", "manu1");
+			session.setAttribute("username2", "manu2");
+			session.setAttribute("username3", "manu3");
+
+			username1 = (String) session.getAttribute("username1");
+			username2 = (String) session.getAttribute("username2");
+			username3 = (String) session.getAttribute("username3");
+
+		} else {
+
+			sessionId = "Existing session accessed: " + session.getId();
+
+			username1 = (String) session.getAttribute("username1");
+			username2 = (String) session.getAttribute("username2");
+			username3 = (String) session.getAttribute("username3");
+
+		}
+
+		String output = """
+				<html>
+				    <body>
+				        <p>username attribute added to the session</p>
+				        """;
+
+		output += "<p>sessionId = " + sessionId + "</p>";
+
+		output += "<p>username1 = " + username1 + "</p>";
+		output += "<p>username2 = " + username2 + "</p>";
+		output += "<p>username3 = " + username3 + "</p>";
+
+		output += "</body></html>";
+
+		return output;
+
+		// Test url: http://localhost:9001/session/create
 
 	}
 
 	@GetMapping("/get")
-	public String getSession(HttpSession session) {
+	public String getSession(HttpServletRequest request) {
 
-		String username = (String) session.getAttribute("username");
+		// Attempt to retrieve existing session without creating a new one
+		HttpSession session = request.getSession(false);
 
-		if (username == null) {
+		String sessionId = "sessionId not found";
+		String username1 = "username1 not found";
+		String username2 = "username2 not found";
+		String username3 = "username3 not found";
 
-			return "No session found!";
+		if (session != null) {
 
+			sessionId = "session accessed: " + session.getId();
+
+			// Retrieve the username attribute from the session
+			username1 = (String) session.getAttribute("username1");
+			username2 = (String) session.getAttribute("username2");
+			username3 = (String) session.getAttribute("username3");
 		}
 
-		return "Session found with username: " + username + " and ID: " + session.getId();
+		String output = """
+				<html>
+				    <body>
+				        <p>username attribute accessed from the session</p>
+				        """;
+
+		output += "<p>sessionId = " + sessionId + "</p>";
+
+		output += "<p>username1 = " + username1 + "</p>";
+		output += "<p>username2 = " + username2 + "</p>";
+		output += "<p>username3 = " + username3 + "</p>";
+		output += "</body></html>";
+
+		return output;
+
+		// Test url: http://localhost:9001/session/get
 
 	}
 
 	@GetMapping("/invalidate")
-	public String invalidateSession(HttpSession session) {
+	public String invalidateSession(HttpServletRequest request) {
 
-		session.invalidate();
+		HttpSession session = request.getSession(false); // Use false to avoid creating a new session if it doesn't
+		// exist);
 
-		return "Session invalidated!";
+
+
+		String output = "session not found";
+
+		if (session != null) {
+
+			// Invalidate the session
+			session.invalidate();
+
+			output = """
+					<html>
+						<body>
+							<p>The session has been invalidated.</p>
+						</body>
+					</html>
+					""";
+		}
+
+		return output;
+
+		// Test url: http://localhost:9001/session/invalidate
 
 	}
 }
