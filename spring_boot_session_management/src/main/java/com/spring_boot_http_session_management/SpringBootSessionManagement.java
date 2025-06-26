ce758e80016b972fa59468e19e799f3ec19452e1
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-06-26 10:57:59 +0530

    Commit changes...

--- spring_boot_session_management/src/main/java/com/spring_boot_http_session_management/SpringBootSessionManagement.java
+++ spring_boot_session_management/src/main/java/com/spring_boot_http_session_management/SpringBootSessionManagement.java
@@ -1,4 +1,4 @@
-package com.spring_boot_session_management;
+package com.spring_boot_http_session_management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- spring_boot_session_management/src/main/java/com/spring_boot_http_session_management/controller/SessionController.java
+++ spring_boot_session_management/src/main/java/com/spring_boot_http_session_management/controller/SessionController.java
@@ -1,4 +1,4 @@
-package com.spring_boot_session_management.controller;
+package com.spring_boot_http_session_management.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
