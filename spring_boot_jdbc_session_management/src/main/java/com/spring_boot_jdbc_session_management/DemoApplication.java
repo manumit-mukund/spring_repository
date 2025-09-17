e6f91ed64e771bc4bc2e266ef07cc96019bf51a6
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-09-17 21:47:16 +0530

    Commit changes...

--- spring_boot_jdbc_session_management/src/main/java/com/spring_boot_jdbc_session_management/DemoApplication.java
+++ spring_boot_jdbc_session_management/src/main/java/com/spring_boot_jdbc_session_management/DemoApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableJdbcHttpSession // It's showing test error.
 public class DemoApplication {
 
 	public static void main(String[] args) {
