c02b25428b409ac6fb9e6520bfe774574407430b
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2025-12-21 18:26:31 +0530

    First commit...

---
+++ spring_boot_circuit_breaker_with_resilience4j/src/main/java/com/spring_boot_circuit_breaker_with_resilience4j/CircuitBreakersResilience4jApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot_circuit_breaker_with_resilience4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitBreakersResilience4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitBreakersResilience4jApplication.class, args);
+	}
+
+}
