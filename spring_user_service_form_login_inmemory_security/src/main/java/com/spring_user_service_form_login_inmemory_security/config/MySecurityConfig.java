d3fb7e44906bf87ccb5dd383da6ae7ed669bae53
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2026-01-10 11:26:10 +0530

    Commit changes..

--- spring_user_service_form_login_inmemory_security/src/main/java/com/spring_user_service_form_login_inmemory_security/config/MySecurityConfig.java
+++ spring_user_service_form_login_inmemory_security/src/main/java/com/spring_user_service_form_login_inmemory_security/config/MySecurityConfig.java
@@ -45,6 +45,7 @@ public class MySecurityConfig {
                 // Register the custom access denied handler
                 .accessDeniedHandler(customAccessDeniedHandler)
             );
+        
         return http.build();
     }
 
