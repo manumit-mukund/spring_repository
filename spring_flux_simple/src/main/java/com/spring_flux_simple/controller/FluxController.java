74ecdcee340bed6280a2567959d7cc13f447c741
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2026-01-14 13:48:22 +0530

    Commit changes...

--- spring_flux_simple/src/main/java/com/spring_flux_simple/controller/FluxController.java
+++ spring_flux_simple/src/main/java/com/spring_flux_simple/controller/FluxController.java
@@ -1,10 +1,15 @@
 package com.spring_flux_simple.controller;
 
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import reactor.core.publisher.Flux;
-import java.time.Duration;
+import reactor.core.publisher.Mono;
 
 @RestController
 public class FluxController {
@@ -20,7 +25,24 @@ public class FluxController {
 		// Create a Flux that emits items "Item 1", "Item 2", "Item 3"
 		// and introduces a delay of 1 second between each emission to simulate a
 		// stream.
-		return Flux.just("Item 1", "Item 2", "Item 3", "Item 4", "Item 5").delayElements(Duration.ofSeconds(2)).log();
+		return Flux
+				.just("Item 1", "Item 2", "Item 3", "Item 4", "Item 5")
+				.delayElements(Duration.ofSeconds(2))
+				.log();
+
+	}
+
+	@GetMapping(value = "/delayed-items", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Flux<String> getDelayedItems() {
+
+		List<String> items = Arrays.asList("Item 1", "Item 2", "Item 3", "Item 4", "Item 5");
+
+		// Use the delayElements operator to introduce a 3-second delay between each
+		// element
+		return Flux
+				.fromIterable(items)
+				.delayElements(Duration.ofSeconds(3))
+				.log(); // The .log() operator provides helpful logs to observe the emissions
 
 	}
 
@@ -33,6 +55,13 @@ public class FluxController {
 
 		// This endpoint will gather all items and return a complete JSON array.
 		return Flux.just("Item 1", "Item 2", "Item 3", "Item 4", "Item 5");
+
+	}
+
+	@GetMapping("/hello")
+	public Mono<String> hello() {
+
+		return Mono.just("Mono: Hello, World!"); // Returns a single, non-blocking response
 
 	}
 }
