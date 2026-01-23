8aac5f8fcb9a1d576796362399bbba84aace2f73
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2026-01-23 22:39:16 +0530

    Commit changes...

--- spring_core_autowire_abstract_class_with_annotation/src/main/java/com/spring_core_autowire_abstract_class_with_annotation/config/BeanConfig.java
+++ spring_core_autowire_abstract_class_with_annotation/src/main/java/com/spring_core_autowire_abstract_class_with_annotation/config/BeanConfig.java
@@ -12,6 +12,7 @@ import com.spring_core_autowire_abstract_class_with_annotation.model.Circle;
 public class BeanConfig {
 
 //	If we want to use Square, we'll have to comment out the Circle bean
+//  If we use use both, we get the error: expected single matching bean but found 2: mySquare,myCircle
 //	@Bean
 //	Square mySquare() {
 //
