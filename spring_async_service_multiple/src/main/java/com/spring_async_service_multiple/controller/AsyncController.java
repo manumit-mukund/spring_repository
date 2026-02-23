7d3b1e818342122d248cfd442451d08961ba6ac4
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2026-02-23 18:14:20 +0530

    Commit changes...

--- spring_async_service_multiple/src/main/java/com/spring_async_service_multiple/controller/AsyncController.java
+++ spring_async_service_multiple/src/main/java/com/spring_async_service_multiple/controller/AsyncController.java
@@ -1,5 +1,7 @@
 package com.spring_async_service_multiple.controller;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.concurrent.CompletableFuture;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,9 +23,19 @@ public class AsyncController {
 	@GetMapping("/multi-async")
 	public CompletableFuture<String> callMultipleAsync() {
 
+		Instant startTime = Instant.now();
+		System.out.println("Process started at: " + startTime);
+
 		CompletableFuture<String> task1 = asyncService.asyncTaskOne();
 		CompletableFuture<String> task2 = asyncService.asyncTaskTwo();
 		CompletableFuture<String> task3 = asyncService.asyncTaskThree();
+
+		Instant endTime = Instant.now();
+		System.out.println("Process ended at: " + endTime);
+
+		Duration timeElapsed = Duration.between(startTime, endTime);
+
+		System.out.println("Total elapsed time in seconds: " + timeElapsed.getSeconds() + " s");
 
 		// Combine all results once they are done
 		return CompletableFuture.allOf(task1, task2, task3)
