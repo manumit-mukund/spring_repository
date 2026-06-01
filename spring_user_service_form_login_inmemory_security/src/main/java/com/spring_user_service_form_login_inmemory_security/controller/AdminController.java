d276d2c36b38efa46f4399d1652ff72fd02554f7
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2026-06-01 11:42:20 +0530

    Commit changes..

--- spring_user_service_form_login_inmemory_security/src/main/java/com/spring_user_service_form_login_inmemory_security/controller/AdminController.java
+++ spring_user_service_form_login_inmemory_security/src/main/java/com/spring_user_service_form_login_inmemory_security/controller/AdminController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +17,7 @@ import com.spring_user_service_form_login_inmemory_security.service.UserService;
 
 @RestController
 @RequestMapping("/admin/users")
-@PreAuthorize("hasRole('ADMIN')")
+//@PreAuthorize("hasRole('ADMIN')") //It's optional as we're already using it in MySecurityConfig
 public class AdminController {
 
 	@Autowired
--- spring_user_service_form_login_inmemory_security/src/main/java/com/spring_user_service_form_login_inmemory_security/controller/UserController.java
+++ spring_user_service_form_login_inmemory_security/src/main/java/com/spring_user_service_form_login_inmemory_security/controller/UserController.java
@@ -1,21 +1,20 @@
 package com.spring_user_service_form_login_inmemory_security.controller;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
-@PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+//@PreAuthorize("hasAnyRole('ADMIN', 'USER')") //It's optional as we're already using it in MySecurityConfig
 public class UserController {
 
 	@GetMapping("/hello")
 	public String hello() {
 
 		return "Hello World for user!";
-		
-		//Test url: http://localhost:9001/user/hello
+
+		// Test url: http://localhost:9001/user/hello
 	}
 
 }
