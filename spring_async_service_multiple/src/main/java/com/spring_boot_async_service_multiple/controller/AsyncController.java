7747dfac9c47792d9869cb6262fb2911585ad57e
manumit_mukund@yahoo.com <manumit_mukund@yahoo.com>
2026-06-28 13:10:27 +0530

    Commit changes...

--- spring_async_service_multiple/src/main/java/com/spring_boot_async_service_multiple/controller/AsyncController.java
+++ spring_async_service_multiple/src/main/java/com/spring_boot_async_service_multiple/controller/AsyncController.java
@@ -1,7 +1,5 @@
 package com.spring_boot_async_service_multiple.controller;
 
-import java.time.Duration;
-import java.time.Instant;
 import java.util.concurrent.CompletableFuture;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,23 +21,27 @@ public class AsyncController {
 	@GetMapping("/multi-async")
 	public CompletableFuture<String> callMultipleAsync() {
 
-		Instant startTime = Instant.now();
+		long startTime = System.currentTimeMillis();
 		System.out.println("Process started at: " + startTime);
 
 		CompletableFuture<String> task1 = asyncService.asyncTaskOne();
 		CompletableFuture<String> task2 = asyncService.asyncTaskTwo();
 		CompletableFuture<String> task3 = asyncService.asyncTaskThree();
 
-		Instant endTime = Instant.now();
-		System.out.println("Process ended at: " + endTime);
-
-		Duration timeElapsed = Duration.between(startTime, endTime);
-
-		System.out.println("Total elapsed time in seconds: " + timeElapsed.getSeconds() + " s");
+		CompletableFuture<Void> completableFutureAllResult = CompletableFuture.allOf(task1, task2, task3);
 
 		// Combine all results once they are done
-		return CompletableFuture.allOf(task1, task2, task3)
+		CompletableFuture<String> finalresult = completableFutureAllResult
 				.thenApply(v -> String.join(" | ", task1.join(), task2.join(), task3.join()));
+
+		long endTime = System.currentTimeMillis();
+		System.out.println("Process ended at: " + startTime);
+
+		long duration = endTime - startTime;
+
+		System.out.println("Total elapsed time in seconds: " + duration + " milliseconds");
+
+		return finalresult;
 
 		// Test url: http://localhost:9001/multi-async
 
